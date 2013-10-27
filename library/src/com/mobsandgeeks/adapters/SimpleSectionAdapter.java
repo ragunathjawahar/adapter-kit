7bd0b6bc47739f1b8bf6c7e4a7f60ed5c9fb913d
Ragunath Jawahar R <ragunathjawahar@gmail.com>
2013-10-27 21:13:20 +0530

    Mavenized library project.

--- library/src/com/mobsandgeeks/adapters/SimpleSectionAdapter.java
+++ library/src/com/mobsandgeeks/adapters/SimpleSectionAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2012 Mobs & Geeks
+ * Copyright (c) 2012 Mobs & Geeks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
